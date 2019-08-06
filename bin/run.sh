6fb9d282470f3778b3c023b0a2e112ef1a409bdc
Gordon MacPherson <gordon@gordonite.tech>
2019-08-06 22:35:32 +0100

    Added util for ninja and tests

---
+++ bin/run.sh
@@ -0,0 +1,13 @@
+echo "watch out for ninja's"
+cd ..
+
+
+if ninja ; then
+	echo "Running tests because we built!"
+	cd bin
+	./run-scale-test.sh
+else
+	cd bin
+	echo "Build failed, tests cannot run!"
+fi
+
