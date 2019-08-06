a88ddb2debc82e2dec4aac52d097c958d9e3a290
Gordon MacPherson <gordon@gordonite.tech>
2019-08-06 21:24:07 +0100

    Single model test

--- bin/run-scale-test.sh
+++ bin/run-scale-test.sh
@@ -1,3 +1,3 @@
-./assimp export Robot.FBX testexport.fbx -lbw -slm -guv -embtex -gs
-./assimp export BIRD.fbx testexport2.fbx -lbw -slm -guv -embtex -gs
+#./assimp export Robot.FBX testexport.fbx -lbw -slm -guv -embtex -gs
+#./assimp export BIRD.fbx testexport2.fbx -lbw -slm -guv -embtex -gs
 ./assimp export fbx/huesitos.fbx testexport3.fbx -lbw -slm -guv -embtex -gs
