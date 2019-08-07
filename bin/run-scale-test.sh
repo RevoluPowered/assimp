ab36c43820da7600d4270a85f8e61bdeb3069696
Gordon MacPherson <gordon@gordonite.tech>
2019-08-07 18:34:59 +0100

    Edited shell script to add new model - text / ascii mode import.

--- bin/run-scale-test.sh
+++ bin/run-scale-test.sh
@@ -1,3 +1,4 @@
 #./assimp export Robot.FBX testexport.fbx -lbw -slm -guv -embtex -gs
 #./assimp export BIRD.fbx testexport2.fbx -lbw -slm -guv -embtex -gs
 ./assimp export fbx/huesitos.fbx testexport3.fbx -lbw -slm -guv -embtex -gs
+#./assimp export fbx/text.fbx testexport4.fbx -lbw -slm -guv -embtex -gs
